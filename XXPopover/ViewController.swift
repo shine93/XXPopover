77f69b83fff3367ec7ee22a9c215a2af4e303671
mengxue.wei <1834093885@qq.com>
2018-07-03 17:47:16 +0800

    加入4个pop，分别针对上、下、左、右四个方向

--- XXPopover/ViewController.swift
+++ XXPopover/ViewController.swift
@@ -12,15 +12,29 @@ class ViewController: UIViewController {
     var blueTipPop : Popover?
 
     @IBOutlet weak var rightPopBtn: UIButton!
-    @IBOutlet weak var clickMeBtn: UIButton!
+    
+    @IBOutlet weak var leftPopBtn: UIButton!
+    
+    @IBOutlet weak var downPopBtn: UIButton!
+    
+    @IBOutlet weak var upPopBtn: UIButton!
+    @IBOutlet weak var rightPopToTopConstraint: NSLayoutConstraint!
+    
+    var popoverOptions: [PopoverOption]!
+    
+    let tipLabel = UILabel()
+    var popType : PopoverType = .right
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
- 
+        
+        configureUI()
     }
     
-    @IBAction func popShow(_ sender: Any) {
-        let popoverOptions: [PopoverOption] = [
-            .type(.right),
+    func popTypeChange(ptype: PopoverType) -> [PopoverOption] {
+        let popType : [PopoverOption] =  [
+            .type(ptype),
             .blackOverlayColor(UIColor(white: 0.0, alpha: 0.0)),
             .arrowSize(CGSize(width: 16, height: 8)),
             .upPadding(20),
@@ -29,26 +43,70 @@ class ViewController: UIViewController {
             .animationIn(TimeInterval(0.2)),
             .sideEdge(8.0)
         ]
-        
-        blueTipPop = Popover(options: popoverOptions, showHandler: {
-            
-        }) {
-//            self.blueTipPop?.removeGestureRecognizer(blueTipPlacesGR)
-        }
-//        let blueTipPlacesGR = UITapGestureRecognizer(target: blueTipPop, action: #selector(blueTipPop?.dismiss))
-//        blueTipPop?.addGestureRecognizer(blueTipPlacesGR)
-//
-        
-        let tipLabel = UILabel()
+        return popType
+    }
+    
+    func configureUI() {
+        //tipLabel
         tipLabel.numberOfLines = 0
         tipLabel.text = "Filter which locations appear on your map with this button"
         tipLabel.textColor = UIColor.white
         tipLabel.frame = CGRect(x: 0, y: 0, width: 176, height: 64)
         tipLabel.font = UIFont.systemFont(ofSize: 12)
         tipLabel.textAlignment = .left
+    }
+   
+    @IBAction func popShow(_ sender: Any) {
         
-        blueTipPop?.show(tipLabel, fromView: rightPopBtn, inView: view)
+        if blueTipPop != nil {
+            blueTipPop?.removeFromSuperview()
+            blueTipPop = nil
+        }
+        
+        popoverOptions = popTypeChange(ptype: .right)
+//        blueTipPop = Popover(options: popoverOptions)
+        blueTipPop = Popover(options: popoverOptions, showHandler: nil, dismissHandler: nil)
+        
+        //blockOverlay = false 时，点击pop本身不会dimiss
+//        blueTipPop?.show(tipLabel, fromView: rightPopBtn, inView: view)
+        blueTipPop?.show(tipLabel, fromView: rightPopBtn, inView: view, blockOverlay: false)
     }
     
+    
+    @IBAction func leftPopShow(_ sender: Any) {
+        if blueTipPop != nil {
+            blueTipPop?.removeFromSuperview()
+            blueTipPop = nil
+        }
+        popoverOptions = popTypeChange(ptype: .left)
+        blueTipPop = Popover(options: popoverOptions)
+        
+        blueTipPop?.show(tipLabel, fromView: leftPopBtn, inView: view, blockOverlay: false)
+
+    }
+    
+    @IBAction func downPopShow(_ sender: Any) {
+        if blueTipPop != nil {
+            blueTipPop?.removeFromSuperview()
+            blueTipPop = nil
+        }
+        popoverOptions = popTypeChange(ptype: .down)
+        blueTipPop = Popover(options: popoverOptions)
+        
+        blueTipPop?.show(tipLabel, fromView: downPopBtn, inView: view, blockOverlay: false)
+
+    }
+    
+    @IBAction func upPopShow(_ sender: Any) {
+        if blueTipPop != nil {
+            blueTipPop?.removeFromSuperview()
+            blueTipPop = nil
+        }
+        popoverOptions = popTypeChange(ptype: .up)
+        blueTipPop = Popover(options: popoverOptions)
+        
+        blueTipPop?.show(tipLabel, fromView: upPopBtn, inView: view, blockOverlay: false)
+
+    }
 }
 
